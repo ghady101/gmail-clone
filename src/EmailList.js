d8dcd11e5da500baf1e654e89a11e55e60bea477
Ghady Gergie <ghadygergie0@gmailcom>
2023-04-17 10:40:19 +0300

    add emailList - emailRow

--- src/EmailList.js
+++ src/EmailList.js
@@ -15,6 +15,7 @@ import {
 } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
 import Section from './Section';
+import EmailRow from './EmailRow';
 
 function EmailList() {
 	return (
@@ -54,8 +55,25 @@ function EmailList() {
 			</div>
 
 			<div className='emailList__list'>
-                
-            </div>
+				<EmailRow
+					title='Github'
+					subject='gmail-clone'
+					description='this is a test #1'
+					time='10pm'
+				/>
+				<EmailRow
+					title='LinkedIn'
+					subject='Ghady-profile'
+					description='this is a test #2, it is a little bit longer than the previous one to check the message fit'
+					time='10pm'
+				/>
+				<EmailRow
+					title='Github'
+					subject='gmail-clone'
+					description='this is a test'
+					time='10pm'
+				/>
+			</div>
 		</div>
 	);
 }
--- src/EmailRow.js
+++ src/EmailRow.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import './EmailRow.css';
+import {
+	CheckBox,
+	LabelImportantOutlined,
+	StarBorderOutlined,
+} from '@mui/icons-material';
+import { IconButton } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 function EmailRow({ id, title, subject, description, time }) {
-	return <div className='emailRow'></div>;
+	const navigate = useNavigate();
+	return (
+		<div onClick={() => navigate('/mail')} className='emailRow'>
+			<div className='emailRow__options'>
+				<CheckBox />
+				<IconButton>
+					<StarBorderOutlined />
+				</IconButton>
+				<IconButton>
+					<LabelImportantOutlined />
+				</IconButton>
+			</div>
+			<h3 className='emailRow__title'>{title}</h3>
+			<div className='emailRow__message'>
+				<h4>
+					{subject}{' '}
+					<span className='emailRow__description'>- {description}</span>
+				</h4>
+			</div>
+			<p className='emailRow__time'>{time}</p>
+		</div>
+	);
 }
 
 export default EmailRow;
